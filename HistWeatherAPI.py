ee86a3e3fe1ecab74364304b3df337bab46d0518
P1811 <p1811@student.jamk.fi>
2020-06-30 13:27:08 +0530

    changed the file output type to csv

--- HistWeatherAPI.py
+++ HistWeatherAPI.py
@@ -10,7 +10,7 @@ import numpy as np #imports numpy module
 loc_keys = ["170353", "174479", "170531", "174504", "174504", "174459", "174030", "169839", "174516", "170541"] #array of location keys of required cities from AccuWeather
 city_names = ["Minden", "Herford", "Hameln", "Bad Salzuflen", "Lohne", "Espelkamp", "Rinteln", "Belm", "Porta Westfalica", "Bunde"]
 
-with open(r"Cleaned96.txt", "a") as f:
+with open(r"Cleaned96.csv", "a") as f:
     f.write("LocationKey, Timestamp, AvgTemperature, WeatherText, HasPrecipitation, PrecipitationType")#prints the header row
     f.write("\n") #prints a blank line so that next hour's data can be viewed clearly
 
@@ -29,7 +29,7 @@ for city in loc_keys: #using for loop to loop through the cities
   prediction =  x["LocalObservationDateTime"], x["Temperature"]["Metric"]["Value"], x["WeatherText"], x["HasPrecipitation"], x["PrecipitationType"] 
   #cleaning the data
  
-  with open(r"Cleaned96.txt", "a") as cleaned: #opening the local file to store the cleaned data
+  with open(r"Cleaned96.csv", "a") as cleaned: #opening the local file to store the cleaned data
    cleaned.write(city+",") #prints the location key into the file
    cleaned.write(", ".join( repr(e) for e in prediction )) #dumps the JSON dictionary in the local file
    cleaned.write("\n") #prints a blank line so that next hour's data can be viewed clearly
